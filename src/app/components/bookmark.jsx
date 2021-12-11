7fe7571f454c065bbde1b9be8de29d45b7ecc489
Ксения Корнеева <user@MacBook-Pro-User.local>
2021-12-11 17:12:05 +0300

    four unit

---
+++ src/app/components/bookmark.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const BookMark = ({ status, ...rest }) => {
+    return (
+        <button {...rest}>
+            <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
+        </button>
+    );
+};
+BookMark.propTypes = {
+    status: PropTypes.bool.isRequired,
+    rest: PropTypes.array.isRequired
+};
+
+export default BookMark;
---
+++ src/app/components/qualitie.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import React from "react";
+const Qualitie = ({ color, name, _id }) => {
+    return (
+        <span className={"badge m-1 bg-" + color} key={_id}>
+            {name}
+        </span>
+    );
+};
+Qualitie.propTypes = {
+    color: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired
+};
+
+export default Qualitie;
